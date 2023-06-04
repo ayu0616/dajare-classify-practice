42ce587dc5a81f4c9fb4afc5853756ad0ab41982
ayu0616 <ayumuogawa0616@gmail.com>
2023-06-04 16:07:02 +0900

    入力をWordで扱うように変更した

--- bag_of_words.py
+++ bag_of_words.py
@@ -8,21 +8,15 @@ class BagOfWords:
         self.tagger = MeCab.Tagger("-Ochasen -d /opt/homebrew/lib/mecab/dic/mecab-ipadic-neologd")
         self.word_set: set[str] = set()
 
-    def add(self, text: str) -> None:
+    def add(self, word: Word) -> None:
         """入力された文章をBOWに追加する
 
         Parameters
         ----------
-        - text: コーパスのテキストの一文
+        - word: 単語
         """
-        res: str = self.tagger.parse(text)
-        for line in res.splitlines():
-            if line == "EOS" or line == "":
-                continue
-            word = Word(line)
-            # part_of_speech = line.split("\t")[3].split("-")[0]
-            if word.is_content_word:
-                self.word_set.add(word.base_form)
+        if word.is_content_word:
+            self.word_set.add(word.base_form)
 
     def assign_id(self) -> None:
         """単語にIDを割り振る"""
@@ -32,19 +26,15 @@ class BagOfWords:
             self.word_to_id[word] = i
             self.id_to_word[i] = word
 
-    def get_vector(self, text: str) -> list[int]:
+    def get_vector(self, sentence: list[Word]) -> list[int]:
         """入力された文章のベクトルを返す
 
         Parameters
         ----------
         - text: テキストの一文
         """
-        res: str = self.tagger.parse(text)
         vector: list[int] = [0] * len(self.word_set)
-        for line in res.splitlines():
-            if line == "EOS" or line == "":
-                continue
-            word = Word(line)
+        for word in sentence:
             try:
                 vector[self.word_to_id[word.base_form]] += 1
             except KeyError:
