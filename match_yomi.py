ae12c91ab61a469669cba5f7a1ceddc90dcdc931
ayu0616 <ayumuogawa0616@gmail.com>
2023-06-05 08:59:01 +0900

    countersの要素が0個でエラーを吐く不具合を修正

--- match_yomi.py
+++ match_yomi.py
@@ -5,4 +5,4 @@ def check(words: list[Word]) -> bool:
     yomi_sentence = "".join(map(lambda word: word.yomi, words))
     seed_candidate_yomi = [word.yomi for word in words if word.is_content_word]
     counters = [yomi_sentence.count(seed) for seed in seed_candidate_yomi]
-    return max(counters) >= 2
+    return len(counters) > 0 and max(counters) >= 2
