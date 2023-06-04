27ce9d1db6b900adbda391855efcb29d955ebb08
ayu0616 <ayumuogawa0616@gmail.com>
2023-06-04 16:14:51 +0900

    入力をWordの配列で受け取るように変更した

--- match_yomi.py
+++ match_yomi.py
@@ -1,13 +1,7 @@
-import MeCab
-
-from setting import CONTENT_WORD_SET
 from word import Word
 
 
-def check(text: str) -> bool:
-    tagger = MeCab.Tagger("-Ochasen -d /opt/homebrew/lib/mecab/dic/mecab-ipadic-neologd")
-    res: str = tagger.parse(text)
-    words = [Word(line) for line in res.splitlines() if line != "EOS"]
+def check(words: list[Word]) -> bool:
     yomi_sentence = "".join(map(lambda word: word.yomi, words))
     seed_candidate_yomi = [word.yomi for word in words if word.is_content_word]
     counters = [yomi_sentence.count(seed) for seed in seed_candidate_yomi]
