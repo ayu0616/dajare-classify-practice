b9ecfc3bc920e1c2e8637080278f2c52e4a9ad04
ayu0616 <ayumuogawa0616@gmail.com>
2023-06-06 22:24:22 +0900

    入力をSentenceで取るように変更した

--- match_yomi.py
+++ match_yomi.py
@@ -1,8 +1,8 @@
-from word import Word
+from word import Sentence
 
 
-def check(words: list[Word]) -> bool:
-    yomi_sentence = "".join(map(lambda word: word.yomi, words))
-    seed_candidate_yomi = [word.yomi for word in words if word.is_content_word]
+def check(sentence: Sentence) -> bool:
+    yomi_sentence = "".join(map(lambda word: word.yomi, sentence))
+    seed_candidate_yomi = [word.yomi for word in sentence if word.is_content_word]
     counters = [yomi_sentence.count(seed) for seed in seed_candidate_yomi]
     return len(counters) > 0 and max(counters) >= 2
