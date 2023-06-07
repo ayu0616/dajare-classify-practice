f241d9c2f31728162c1c58b8e5694677639d6fec
ayu0616 <ayumuogawa0616@gmail.com>
2023-06-07 14:37:29 +0900

    読み仮名完全一致を調べるときに重複する単語を削除するように変更

--- match_yomi.py
+++ match_yomi.py
@@ -2,7 +2,8 @@ from word import Sentence
 
 
 def check(sentence: Sentence) -> bool:
-    yomi_sentence = "".join(map(lambda word: word.yomi, sentence))
+    sentence_set = set(sentence)  # 同じ単語を削除
+    yomi_sentence = "".join(map(lambda word: word.yomi, sentence_set))
     seed_candidate_yomi = [word.yomi for word in sentence if word.is_content_word]
     counters = [yomi_sentence.count(seed) for seed in seed_candidate_yomi]
     return len(counters) > 0 and max(counters) >= 2
