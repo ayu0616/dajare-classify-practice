f7418e5423d58f1edde4cce1833ea54a392d22ae
ayu0616 <ayumuogawa0616@gmail.com>
2023-06-08 16:11:42 +0900

    1文字の単語を省くように変更した

--- match_yomi.py
+++ match_yomi.py
@@ -2,8 +2,7 @@ from word import Sentence
 
 
 def check(sentence: Sentence) -> bool:
-    sentence_set = set(sentence)  # 同じ単語を削除
-    yomi_sentence = "".join(map(lambda word: word.yomi, sentence_set))
-    seed_candidate_yomi = [word.yomi for word in sentence if word.is_content_word]
-    counters = [yomi_sentence.count(seed) for seed in seed_candidate_yomi]
-    return len(counters) > 0 and max(counters) >= 2
+    yomi_sentence = "".join(map(lambda word: word.yomi, sentence))
+    seed_candidate_yomi = [word.yomi for word in sentence if word.is_content_word and len(word.yomi) >= 2]
+    counters = [yomi_sentence.count(seed) - seed_candidate_yomi.count(seed) for seed in seed_candidate_yomi]
+    return len(counters) > 0 and max(counters) >= 1
