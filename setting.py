6aed93b27aa17c88e0c8eac9fb4fbdcb412e2292
ayu0616 <ayumuogawa0616@gmail.com>
2023-06-10 10:33:09 +0900

    DIC_DIRが存在しないときにはエラーを吐くように変更

--- setting.py
+++ setting.py
@@ -4,5 +4,9 @@ import dotenv
 
 dotenv.load_dotenv()
 
-DIC_DIR = os.getenv("DIC_DIR")  # 辞書ディレクトリ
+__DIC_DIR = os.getenv("DIC_DIR")  # 辞書ディレクトリ
+if __DIC_DIR is None:
+    raise EnvironmentError("DIC_DIR is not set")
+DIC_DIR = __DIC_DIR
+
 CONTENT_WORD_SET = {"名詞", "固有名詞", "動詞", "形容詞", "副詞", "感動詞"}  # 内容語の品詞リスト
