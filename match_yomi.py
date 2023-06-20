994d8677d451ae5522085056029ba2646363fd29
ayu0616 <ayumuogawa0616@gmail.com>
2023-06-20 19:16:24 +0900

    アルゴリズムを微修正した

--- match_yomi.py
+++ match_yomi.py
@@ -3,6 +3,6 @@ from word import Sentence
 
 def check(sentence: Sentence) -> bool:
     yomi_sentence = "".join(map(lambda word: word.yomi, sentence))
-    seed_candidate_yomi = [word.yomi for word in sentence if word.is_content_word and len(word.yomi) >= 2]
-    counters = [yomi_sentence.count(seed) - seed_candidate_yomi.count(seed) for seed in seed_candidate_yomi]
+    seed_candidate = [word for word in sentence if word.is_content_word and len(word.moras) >= 2]
+    counters = [yomi_sentence.count(seed.yomi) - seed_candidate.count(seed) for seed in seed_candidate]
     return len(counters) > 0 and max(counters) >= 1
