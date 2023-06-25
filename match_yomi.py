9b435eead761c844f58c642025acea56703f0855
ayu0616 <ayumuogawa0616@gmail.com>
2023-06-25 15:29:24 +0900

    モーラで読み仮名をチェックするように変更した

--- match_yomi.py
+++ match_yomi.py
@@ -2,7 +2,13 @@ from word import Sentence
 
 
 def check(sentence: Sentence) -> bool:
-    yomi_sentence = "".join(map(lambda word: word.yomi, sentence))
+    moras = [mora for word in sentence for mora in word.moras]
     seed_candidate = [word for word in sentence if word.is_content_word and len(word.moras) >= 2]
-    counters = [yomi_sentence.count(seed.yomi) - seed_candidate.count(seed) for seed in seed_candidate]
-    return len(counters) > 0 and max(counters) >= 1
+    cnt = 0
+    for seed in seed_candidate:
+        tmp_cnt = 1 - seed_candidate.count(seed)
+        for i in range(len(moras) - len(seed.moras) + 1):
+            if moras[i : i + len(seed.moras)] == seed.moras:
+                tmp_cnt += 1
+        cnt = max(cnt, tmp_cnt)
+    return cnt >= 2
